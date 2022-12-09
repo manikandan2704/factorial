8571f7a351415b4008cbde4cbe57b439e458039a
manikandan <manimani19508@gmail.com>
2022-12-09 15:27:04 +0530

    add new file

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+var num=10;
+var num1=0;
+var number1=0;
+var number2=1;
+console.log(number1,number2);
+for(i=0;i<num;i++){
+    num1=number1+number2
+    number1=number2
+    number2=num1
+    console.log(num1)
+}
